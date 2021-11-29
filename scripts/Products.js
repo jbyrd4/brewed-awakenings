56164be6c7cf88163a99133d6c11de3b1d20c7df
jbyrd4 <jbyrd4@gmail.com>
2021-11-29 15:59:44 -0600

    chapter 4 and 5 complete

--- scripts/Products.js
+++ scripts/Products.js
@@ -14,3 +14,16 @@ export const Products = () => {
     return html
 }
 
+document.addEventListener ("click", (clickEvent) => {
+    const itemClicked = clickEvent.target
+    console.log(itemClicked.id)
+    if (itemClicked.id.startsWith("product")) {
+        const [,productID] = itemClicked.id.split("--")
+
+        for (const product of products) {
+            if (product.id === parseInt(productID)) {
+                window.alert(`The ${product.name} costs: $${product.price.toFixed(2)}`)
+            }
+        }
+    }
+})
